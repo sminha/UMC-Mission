68d72de242f1bf2844f9f97397ad89029a884ef8
sminha <sminha8590@naver.com>
2023-11-23 15:18:26 +0900

    chapter8

---
+++ spring/spring/src/main/java/umc/spring/study/converter/MemberPreferConverter.java
@@ -0,0 +1,22 @@
+package umc.spring.study.converter;
+
+import umc.spring.study.domain.FoodCategory;
+import umc.spring.study.domain.Member;
+import umc.spring.study.domain.mapping.MemberPrefer;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class MemberPreferConverter {
+
+    public static List<MemberPrefer> toMemberPreferList(List<FoodCategory> foodCategoryList){
+
+        return foodCategoryList.stream()
+                .map(foodCategory ->
+                        MemberPrefer.builder()
+                                .foodCategory(foodCategory)
+                                .build()
+                ).collect(Collectors.toList());
+    }
+}
+
